d6a0a6a4b1fddc995480aa2df72fef1cf0ccc4e7
Derek Scharton <dscharton@financialengines.com>
2017-09-08 19:10:52 -0700

    cleanup of code from old project

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 (function () {
     'use strict';
     var app = angular.module('app', []);
-    app.controller('ctrl', function (strings, beermenu, $http, $log) {
+    app.controller('ctrl', function ($http, $log) {
         var vm = this;
-        vm.headtitle = strings.headtitle;
-        vm.name = strings.name;
-        vm.address = strings.address;
-        vm.phone = strings.phone;
-        vm.email = strings.email;
-        vm.facebook = strings.facebook;
-        vm.instagram = strings.instagram;
-        vm.gatrackingid = strings.gatrackingid;
-        vm.hours = strings.hours;
-        vm.beermenu = beermenu;
+        vm.headtitle = 'TechYes!!'
 
         var beerlist;
         $http.get('models/misfits.json').then(function(result){
